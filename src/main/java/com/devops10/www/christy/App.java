ebb83f9fde32bb3559d8abb8e4f3ce29d79f1c4d
ChristySojan <sojanchristy@gmail.com>
2025-04-25 14:40:21 +0530

    first commit

---
+++ src/main/java/com/devops10/www/christy/App.java
@@ -0,0 +1,13 @@
+package com.devops10.www.christy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
